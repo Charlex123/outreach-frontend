541b2be56c2a2599087f3d69b700e5920c4cb9cb
2036380 <charlesmuoka1@gmail.com>
2023-08-04 01:17:38 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome, faGoogle } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, faTwitter, faFontAwesome, faGoogle)
 
 function App() {
+
+  const handleClick = () => {
+    window.location.href = process.env.REACT_APP_REDIRECT!
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Welcome To The Outreach
+        </h1>
+        <h2>Your best mass gmail sender</h2>
+
+        <div className='signin_btn'>
+            <button type='button' onClick={handleClick}><FontAwesomeIcon icon={faGoogle} size='lg' className='google-signin-icon'/> Sign In To Gmail To Continue</button>
+        </div>
     </div>
   );
 }
