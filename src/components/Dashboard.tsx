9bc0f1bb213d2e995c5a6838bb38b83683a09ece
2036380 <charlesmuoka1@gmail.com>
2023-08-17 11:45:51 +0100

    another commit

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Overlay } from '../App'
+import axios from 'axios';
 import Modal from './Modal'
 import FileUploadModal from './Modal/FileUploadModal';
 import { OutreachButton } from './styles/ButtonVariants.styled';
@@ -9,8 +10,29 @@ const Dashboard = () => {
    const [recipientModalState, setRecipientModalState] = useState<boolean>(false);
    const [bulkRecipients, setBulkRecipients] = useState<string>("");
    const [recipientsInputType, seRecipientsInputType] = useState<string>("");
+   const [userappkey, setAppKey] = useState<any>("");
 
+   console.log('user app key',setAppKey(localStorage.getItem("signature")));
+   async function getUserdata() {
+      try {
+         const config = {
+           headers: {
+             "Content-type": "application/json"
+           }
+         }  
+         const {data} = await axios.post("/user/verifyuserdata", {
+            userappkey
+          }, config);
+         console.log('data -', data)
+         localStorage.setItem("userData_", JSON.stringify(data))
+         console.log('user data --',localStorage.getItem("userData_"))
+       } catch (error) {
+         console.log(error)
+       }
+   }
    
+   getUserdata();
+
    const showModal = (e: React.MouseEvent): void => setModalState(true);
    return (
       <Container>
