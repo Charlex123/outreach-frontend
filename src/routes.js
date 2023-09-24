c5ceed685809c4513c52d4b7260d975fe2a4283e
2036380 <charlesmuoka1@gmail.com>
2023-09-24 22:06:24 +0100

    another commit

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import Social from "./components/Social";
 import Dashboard from "./components/Dashboard";
 import Login from './components/Login';
-// import Privacy from './components/Privacy';
-import Terms from './components/Terms';
+import Privacy from './components/privacy';
+import Terms from './components/terms';
 
 
 // ----------------------------------------------------------------------
@@ -12,7 +12,7 @@ export default function Router() {
   return useRoutes([
     { path: '/dashboard', element: <Dashboard /> },
     { path: '/social', element: <Social /> },
-    // { path: '/privacy-policy', element: <Privacy /> },
+    { path: '/privacy-policy', element: <Privacy /> },
     { path: '/terms', element: <Terms /> },
     { path: '/', element: <Login /> },
   ]);
